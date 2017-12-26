116a6ca662f3df59a1b15dbecbcb03e7fbba4422
AlejandroTrejoCode <alejandrotrejocode@gmail.com>
2017-12-26 16:10:49 -0500

    Cambios del SendEmail

--- sendMessage.php
+++ sendMessage.php
@@ -11,19 +11,18 @@ $email = strtolower($email);
 	$mail->CharSet = 'UTF-8';
 	$mail->SMTPDebug = 2; // enables SMTP debug
 	//$mail->SMTPSecure = "ssl";
-	$mail->Host = "mail.chicleypegacreativo.com"; // SMTP a utilizar. Por ej. mtp.elserver.com
-	$mail->Username = "sergio@chicleypegacreativo.com"; // Correo completo a utilizar
-	$mail->Password = "sergio1234"; // Contraseña
+	$mail->Host = " "; // SMTP a utilizar. Por ej. mtp.elserver.com
+	$mail->Username = " "; // Correo completo a utilizar
+	$mail->Password = " "; // Contraseña
 	$mail->Port = 25; // Puerto a utilizar
 
 	//Con estas pocas líneas iniciamos una conexión con el SMTP. Lo que ahora deberíamos hacer, es configurar el mensaje a enviar, el //From, etc.
-	$mail->From = "sergio@chicleypegacreativo.com"; // Desde donde enviamos (Para mostrar)
-	$mail->FromName = "Template";
+	$mail->From = " "; // Desde donde enviamos (Para mostrar, correo)
+	$mail->FromName = " Nombre ";
 
 	//Estas dos líneas, cumplirían la función de encabezado (En mail() usado de esta forma: “From: Nombre <correo@dominio.com>”) de //correo.
 	$mail->IsHTML(true); // El correo se envía como HTML
-	$mail->Subject = "¡Información solicitada - Franquicias!"; // Este es el titulo del email.
-	$body = 'Template';
+	$mail->Subject = " Sujeto "; // Este es el titulo del email.
 	$mail->msgHTML(file_get_contents('template.html'), dirname(__FILE__)); // Mensaje a enviar
 	$mail-> AddAddress($email);
 
